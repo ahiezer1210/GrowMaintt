886e9ea1074a4f69b99a70e7769f0e698b15e73d
Abi <estelitaabigail2008@gmail.com>
2026-08-16 12:37:45 -0600

    Agregar ejemplo de configuración Firebase

---
+++ firebaseconfi.example.js
@@ -0,0 +1,31 @@
+/*
+  Este es el ejemplo base para la Configuración 
+  de Firebase para conectar la aplicación
+  con la base de datos, poner las credenciales 
+  de Firebase segun el mensaje enviado.
+*/
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+const db = getFirestore(app);
+
+export { auth, db };
+ 
+ 
